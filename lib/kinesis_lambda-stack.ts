bbcc0f7b90ae3d1b9a917f28e12c6d0b5cc6b667
uthx <rdrahuldhiman.19@gmail.com>
2023-01-18 08:59:43 +0530

    Implemented Kinesis and Lambda changes

--- lib/kinesis_lambda-stack.ts
+++ lib/kinesis_lambda-stack.ts
@@ -1,6 +1,11 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as kinesis from 'aws-cdk-lib/aws-kinesis';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
+import { StartingPosition } from 'aws-cdk-lib/aws-lambda';
+// import * as kinesis from '@aws-cdk/aws-kinesis';
+
 
 export class KinesisLambdaStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -12,5 +17,20 @@ export class KinesisLambdaStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'KinesisLambdaQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+    const stream = new kinesis.Stream(this, 'MyKinesisStream', {
+      streamName: 'MyKinesisStream'
+    })
+    const lambdaFunction = new lambda.Function(this, 'Function', {
+      code: lambda.Code.fromAsset('src'),
+      handler: 'index.handler',
+      functionName: 'KinesisMessageHandler',
+      runtime: lambda.Runtime.NODEJS_14_X,
+    });
+
+    const eventSource = new lambdaEventSources.KinesisEventSource(stream, {
+      startingPosition: lambda.StartingPosition.TRIM_HORIZON,
+    });
+
+    lambdaFunction.addEventSource(eventSource);
   }
 }
